d0ce5e635af287ba9e8cac8926715515384fe21c
Frédéric Delorme <frederic.delorme@snapgames.fr>
2017-09-13 00:02:19 +0200

    Create repository for GDJ102

---
+++ README.md
@@ -0,0 +1,33 @@
+# GDJ102
+
+[![TravisCI](https://travis-ci.org/SnapGames/GDJ102.svg?branch=develop)](https://travis-ci.org/SnapGames/GDJ102 "open the TravisCI compilation trend")
+
+## Game Developement Java Basics 101
+
+This project i spart of the [GDJ102](https://classroom.google.com/c/NzI2ODQ3NjU2MFpa/t/NzI2Nzg0MjgxNFpa) course from [GameDev Basics
+Java](https://classroom.google.com/c/NzI2ODQ3NjU2MFpa "Open the official online course") 
+provided by the [SnapGames](http://snapgames.fr) site. 
+
+## Compile
+
+To compile the full project, please execute the followin' command :
+
+    $> mvn clean install
+
+
+## Execute
+
+to execute the the compiled jar, please execute the command bellow :
+
+    $> mvn exec:exec
+
+
+## Edit
+
+Import this project as an Existing Maven Project into your prefered IDE, 
+(like [Eclipse](http://www.eclipse.org/downloads "open the eclipse official web download page") ?)
+
+
+Have Fun !
+
+[Frédéric Delorme](mailto:contact@snapgames.fr?subject=gdj102 "send a mail to your tutor")
---
+++ src/main/java/com/snapgames/gdj/gdj102/Game.java
@@ -0,0 +1,71 @@
+/**
+ * SnapGames
+ * 
+ * Game Development Java
+ * 
+ * GDJ102
+ * 
+ * @year 2017
+ */
+package com.snapgames.gdj.gdj102;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+/**
+ * the basic Game container is a JPanel child.
+ * 
+ * @author Frederic
+ *
+ */
+public class Game extends JPanel {
+
+	/**
+	 * The title for the game instance.
+	 */
+	private String title = "game";
+
+	private Dimension dimension = null;
+
+	/**
+	 * the default constructor for the {@link Game} panel with a game
+	 * <code>title</code>.
+	 * 
+	 * @param title
+	 *            the title for the game.
+	 */
+	private Game(String title) {
+		this.title = title;
+		this.dimension = new Dimension(640, 400);
+	}
+
+	/**
+	 * return the title of the game.
+	 * 
+	 * @return
+	 */
+	public String getTitle() {
+		return this.title;
+	}
+
+	/**
+	 * return the dimension of the Game display.
+	 * 
+	 * @return
+	 */
+	public Dimension getDimension() {
+		return dimension;
+	}
+
+	/**
+	 * The main entry point to start our GDJ101 game.
+	 * 
+	 * @param argv
+	 */
+	public static void main(String[] argv) {
+		Game game = new Game("GDJ101");
+		Window window = new Window(game);
+	}
+
+}
